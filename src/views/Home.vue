7c2f6b10c1c76b121f362584e0a3ac9fe7be77ee
Sam Garcia <samangarc22@gmail.com>
2022-06-25 10:29:25 -0400

    Simple artist card set up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,12 @@
 <script setup>
 import ButtonRepo from '@/components/ButtonRepo.vue'
+import ArtistCard from '@/components/ArtistCard.vue'
 </script>
 
 <template>
   <div class="bg-gray-50">
-    <div
+    <ArtistCard />
+    <!-- <div
       class="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8"
     >
       <h2
@@ -24,6 +26,6 @@ import ButtonRepo from '@/components/ButtonRepo.vue'
         </div>
         <ButtonRepo />
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
