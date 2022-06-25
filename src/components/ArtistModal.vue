493ed8e051d20f75940721a329e4db205edd7fd4
Sam Garcia <samangarc22@gmail.com>
2022-06-25 17:37:05 -0400

    Connect Wallet to Vote Functionality

--- src/components/ArtistModal.vue
+++ src/components/ArtistModal.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import 'vue3-carousel/dist/carousel.css';
+import { connectWallet } from "../utils/web3wallet";
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 
 export default {
@@ -16,7 +17,10 @@ export default {
         closeModal() {
             this.$emit('closedModal');
         },
-        selectArtist() {
+        async selectArtist() {
+            if (this.$store.state.provider == null) {
+                await connectWallet();
+            }
             this.$emit('selectedArtist');
         }
     },
@@ -67,7 +71,10 @@ export default {
             </div>
             </div>
             <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse justify-center">
-                <button type="button" @click="selectArtist" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">Select</button>
+
+                <button type="button" @click="selectArtist" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+                    {{this.$store.state.provider == null ? "Connect Wallet To Vote" : "Select"}}
+                </button>
                 <button type="button" @click="closeModal" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Go back</button>
             </div>
         </div>
