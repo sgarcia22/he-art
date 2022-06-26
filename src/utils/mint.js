87e8da1e11a34e6656746796e734c264a69c8089
Sam Garcia <samangarc22@gmail.com>
2022-06-25 22:06:00 -0400

    Update mint.js

--- src/utils/mint.js
+++ src/utils/mint.js
@@ -14,15 +14,15 @@ export async function mintNFT (name, description, image) {
     // Upload metadata to IPFS
     const ipfsMetadataHash = await ipfsUpload(json);
 
+    const contractAddress = "0xA6CE7eB08A19Ed0331410b45866E7F71D9BB1f0b";
+
     // Mint NFT on Polygon
     const transactionHash = await mintNFTWithUri(false, {
-        to: '0x687422eEA2cB73B5d3e242bA5456b782919AFc85',
-        url: 'ipfs://bafybeidi7xixphrxar6humruz4mn6ul7nzmres7j4triakpfabiezll4ti/metadata.json',
+        to: contractAddress,
+        url: ipfsMetadataHash,
         tokenId: '100000',
         chain: Currency.MATIC,
-        contractAddress: '0x687422eEA2cB73B5d3e242bA5456b782919AFc85',
-        fromPrivateKey: '0x05e150c73f1920ec14caa1e0b6aa09940899678051a78542840c2668ce5080c2'
+        contractAddress: contractAddress,
+        fromPrivateKey: process.env.VITE_MUMBAI_API_KEY
     });
-
-    // Upload to Opensea
 }
